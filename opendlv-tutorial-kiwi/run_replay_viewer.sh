45bea2c1a1c55bc80998d5383f7d0d8fcf4250b3
Tony <amk1997@hotmail.se>
2020-05-03 16:09:38 +0200

    first commit

---
+++ opendlv-tutorial-kiwi/run_replay_viewer.sh
@@ -0,0 +1,5 @@
+while true
+do
+docker-compose -f ./opendlv-perception-helloworld-cpp/h264-replay-viewer.yml up
+sleep 1
+done
---
+++ opendlv-tutorial-kiwi/start.sh
@@ -0,0 +1,2 @@
+docker build -f ./opendlv-perception-helloworld-cpp/Dockerfile -t myapp ./opendlv-perception-helloworld-cpp/
+docker-compose -f opendlv-perception-helloworld-cpp/myapp.yml up
