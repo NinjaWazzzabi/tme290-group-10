c246cf225ed90438c7f4b5fa6df687040d8dcb88
Landsfadern <elias.w.burstrom@gmail.com>
2020-05-27 21:25:24 +0200

    Add fully functional kiwi detection using Yolov3-tiny and opencv

--- kiwi-detection/src/main.cpp
+++ kiwi-detection/src/main.cpp
@@ -4,6 +4,7 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/objdetect.hpp>
+#include <opencv2/dnn.hpp>
 
 #include <stdint.h>
 #include <iostream>
@@ -11,6 +12,25 @@
 #include <mutex>
 
 using namespace cv;
+using namespace dnn;
+using namespace std;
+
+// Initialize the parameters
+float confThreshold = 0.3; // Confidence threshold
+float nmsThreshold = 0.2;  // Non-maximum suppression threshold
+int inpWidth = 608;  // Width of network's input image
+int inpHeight = 608; // Height of network's input image
+std::vector<std::string> classes{"kiwi"};
+
+// Remove the bounding boxes with low confidence using non-maxima suppression
+void postprocess(Mat& frame, const std::vector<Mat>& out);
+
+// Draw the predicted bounding box
+void drawPred(int classId, float conf, int left, int top, int right, int bottom, Mat& frame);
+
+// Get the names of the output layers
+vector<String> getOutputsNames(const Net& net);
+
 
 int32_t main(int32_t, char **)
 {
@@ -22,10 +42,14 @@ int32_t retCode{1};
 	const uint32_t HEIGHT{720};
 	//const bool VERBOSE{true};
 	const uint16_t CID{111};
-	//const Point CAMERA_POS = Point(WIDTH/2, HEIGHT);
-	CascadeClassifier face_cascade;
-	if(!face_cascade.load( "/usr/bin/cascade.xml" ))
-		std::cout << "ERROR in loading Cascade" << std::endl;
+
+	String modelConfiguration = "kiwi.cfg";
+    String modelWeights = "kiwi_5000.weights";
+
+    // Load the network
+    Net net = readNetFromDarknet(modelConfiguration, modelWeights);
+    net.setPreferableBackend(DNN_BACKEND_OPENCV);
+    net.setPreferableTarget(DNN_TARGET_CPU);
 
 
 	// Attach to the shared memory.
@@ -93,30 +117,137 @@ int32_t retCode{1};
 				img = wrapped.clone();
 			}
 			sharedMemory->unlock();
+			Mat frame, blob;
+			cvtColor(img, frame, 1);
+
+			// Create a 4D blob from a frame.
+			blobFromImage(frame, blob, 1/255.0, Size(inpWidth, inpHeight), Scalar(0,0,0), true, false);
+
+			
+			
+			//Sets the input to the network
+			net.setInput(blob);
+			
+			// Runs the forward pass to get output of the output layers
+			vector<Mat> outs;
+			net.forward(outs, getOutputsNames(net));
+			
+			// Remove the bounding boxes with low confidence
+			postprocess(frame, outs);
+			
+			// Put efficiency information. The function getPerfProfile returns the overall time for inference(t) and the timings for each of the layers(in layersTimes)
+			vector<double> layersTimes;
+			double freq = getTickFrequency() / 1000;
+			double t = net.getPerfProfile(layersTimes) / freq;
+			string label = format("Inference time for a frame : %.2f ms", t);
+			putText(frame, label, Point(0, 15), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 0, 255));
+			
+			// Write the frame with the detection boxes
+			Mat detectedFrame;
+			frame.convertTo(detectedFrame, CV_8U);
 
 
-			//Rect myROI(0, img.rows*8/16, img.cols, img.rows*4.2/16);
-			//Mat croppedImage = img(myROI);
+			//for( size_t i = 0; i < kiwis.size(); i++ )
+			//{
+			//	cv::rectangle(img, Point(kiwis[i].x,kiwis[i].y), Point(kiwis[i].x + kiwis[i].width, kiwis[i].y + kiwis[i].height), Scalar(255,0,0));
+			//}
 
 
-
-			Mat  gray;
-			cv::cvtColor(img , gray , cv::COLOR_BGR2GRAY );
-
-			std::vector<Rect> kiwis;
-			face_cascade.detectMultiScale( gray, kiwis, 1.1, 2, 0, Size(60, 40), Size(300,280) );
-
-			for( size_t i = 0; i < kiwis.size(); i++ )
-			{
-				cv::rectangle(img, Point(kiwis[i].x,kiwis[i].y), Point(kiwis[i].x + kiwis[i].width, kiwis[i].y + kiwis[i].height), Scalar(255,0,0));
-			}
-
-
-			imshow("kiwis", img);
+			imshow("kiwis", frame);
 			waitKey(1);
 
 		}
 	}
 	retCode = 0;
 	return retCode;
+}
+// Remove the bounding boxes with low confidence using non-maxima suppression
+void postprocess(Mat& frame, const vector<Mat>& outs)
+{
+    vector<int> classIds;
+    vector<float> confidences;
+    vector<Rect> boxes;
+    
+    for (size_t i = 0; i < outs.size(); ++i)
+    {
+        // Scan through all the bounding boxes output from the network and keep only the
+        // ones with high confidence scores. Assign the box's class label as the class
+        // with the highest score for the box.
+        float* data = (float*)outs[i].data;
+        for (int j = 0; j < outs[i].rows; ++j, data += outs[i].cols)
+        {
+            Mat scores = outs[i].row(j).colRange(5, outs[i].cols);
+            Point classIdPoint;
+            double confidence;
+            // Get the value and location of the maximum score
+            minMaxLoc(scores, 0, &confidence, 0, &classIdPoint);
+            if (confidence > confThreshold)
+            {
+                int centerX = (int)(data[0] * frame.cols);
+                int centerY = (int)(data[1] * frame.rows);
+                int width = (int)(data[2] * frame.cols);
+                int height = (int)(data[3] * frame.rows);
+                int left = centerX - width / 2;
+                int top = centerY - height / 2;
+                
+                classIds.push_back(classIdPoint.x);
+                confidences.push_back((float)confidence);
+                boxes.push_back(Rect(left, top, width, height));
+            }
+        }
+    }
+    
+    // Perform non maximum suppression to eliminate redundant overlapping boxes with
+    // lower confidences
+    vector<int> indices;
+    NMSBoxes(boxes, confidences, confThreshold, nmsThreshold, indices);
+    for (size_t i = 0; i < indices.size(); ++i)
+    {
+        int idx = indices[i];
+        Rect box = boxes[idx];
+        drawPred(classIds[idx], confidences[idx], box.x, box.y,
+                 box.x + box.width, box.y + box.height, frame);
+    }
+}
+
+// Draw the predicted bounding box
+void drawPred(int classId, float conf, int left, int top, int right, int bottom, Mat& frame)
+{
+    //Draw a rectangle displaying the bounding box
+    rectangle(frame, Point(left, top), Point(right, bottom), Scalar(255, 178, 50), 3);
+    
+    //Get the label for the class name and its confidence
+    string label = format("%.2f", conf);
+    if (!classes.empty())
+    {
+        CV_Assert(classId < (int)classes.size());
+        label = classes[classId] + ":" + label;
+    }
+    
+    //Display the label at the top of the bounding box
+    int baseLine;
+    Size labelSize = getTextSize(label, FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseLine);
+    top = max(top, labelSize.height);
+    rectangle(frame, Point(left, top - round(1.5*labelSize.height)), Point(left + round(1.5*labelSize.width), top + baseLine), Scalar(255, 255, 255), FILLED);
+    putText(frame, label, Point(left, top), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(0,0,0),1);
+}
+
+// Get the names of the output layers
+vector<String> getOutputsNames(const Net& net)
+{
+    static vector<String> names;
+    if (names.empty())
+    {
+        //Get the indices of the output layers, i.e. the layers with unconnected outputs
+        vector<int> outLayers = net.getUnconnectedOutLayers();
+        
+        //get the names of all the layers in the network
+        vector<String> layersNames = net.getLayerNames();
+        
+        // Get the names of the output layers in names
+        names.resize(outLayers.size());
+        for (size_t i = 0; i < outLayers.size(); ++i)
+        names[i] = layersNames[outLayers[i] - 1];
+    }
+    return names;
 }
