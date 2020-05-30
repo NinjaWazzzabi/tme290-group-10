7422e7a4c80757128d955415bc8a7e3b3ca7f710
Landsfadern <elias.w.burstrom@gmail.com>
2020-05-30 17:05:43 +0200

    Implement action-planner to handle intersections with other kiwis

--- action-planner/src/main.cpp
+++ action-planner/src/main.cpp
@@ -6,13 +6,14 @@
 #include <mutex>
 #include "cone_location.hpp"
 #include "serializer.hpp"
+#include "kiwi_location.hpp"
 
 #define CID 111
 #define FREQ 10
 
 #define T_LEFT  -20
 #define T_RIGHT 60
-#define T_TIME 0.5f
+#define T_WAIT_TIME 1000000 //One second
 
 #define DRIVE 0
 #define STOP 1
@@ -21,8 +22,9 @@ int32_t main(int32_t, char **)
 {
 	std::mutex m_external_data;
 	std::vector<ConeLocation> global_cones;
-	opendlv::robo::TrafficLocation global_traffic_msg;
-	uint32_t global_traffic_msg_timestamp;
+	std::vector<KiwiLocation> global_kiwis;
+	opendlv::robo::KiwiLocation global_traffic_msg;
+	cluon::data::TimeStamp last_traffic_msg_timestamp;
 
 	auto cone_list_listener{[&global_cones, &m_external_data](cluon::data::Envelope &&envelope) {
 		auto cones_message = cluon::extractMessage<opendlv::robo::ConeLocation>(std::move(envelope));
@@ -34,30 +36,41 @@ int32_t main(int32_t, char **)
 		}
 	}};
 
-	auto traffic_listener{[&global_traffic_msg, &global_traffic_msg_timestamp, &m_external_data](cluon::data::Envelope &&envelope) {
-		auto traffic_msg = cluon::extractMessage<opendlv::robo::TrafficLocation>(std::move(envelope));
+	// Remove?
+	//auto traffic_listener{[&global_traffic_msg, &global_traffic_msg_timestamp, &m_external_data](cluon::data::Envelope &&envelope) {
+	//	auto traffic_msg = cluon::extractMessage<opendlv::robo::TrafficLocation>(std::move(envelope));
+	//	{
+	//		std::lock_guard<std::mutex> lock(m_external_data);
+	//		global_traffic_msg_timestamp = cluon::time::toMicroseconds(cluon::time::now());
+	//		global_traffic_msg = traffic_msg;
+	//	}
+	//}};
+
+	
+	auto kiwi_list_listener{[&global_kiwis, &m_external_data](cluon::data::Envelope &&envelope) {
+		auto kiwis_message = cluon::extractMessage<opendlv::robo::KiwiLocation>(std::move(envelope));
+		std::vector<KiwiLocation> kiwis = Serializer::decode<KiwiLocation>(kiwis_message.data());
 		{
 			std::lock_guard<std::mutex> lock(m_external_data);
-			global_traffic_msg_timestamp = cluon::time::toMicroseconds(cluon::time::now());
-			global_traffic_msg = traffic_msg;
+			global_kiwis.clear();
+			global_kiwis = kiwis;
 		}
 	}};
 
 	cluon::OD4Session od4{CID};
 	od4.dataTrigger(opendlv::robo::ConeLocation::ID(), cone_list_listener);
-	od4.dataTrigger(opendlv::robo::TrafficLocation::ID(), traffic_listener);
+	od4.dataTrigger(opendlv::robo::KiwiLocation::ID(), kiwi_list_listener);
 
 	auto decision_runner{[&]() -> bool {
 
 		// Copy variables to local scope
 		std::vector<ConeLocation> cones;
-		opendlv::robo::TrafficLocation traffic_msg;
-		uint32_t traffic_msg_timestamp = 0;
+		std::vector<KiwiLocation> kiwis;
+		cluon::data::TimeStamp traffic_msg_timestamp;
 		{
 			std::lock_guard<std::mutex> lock(m_external_data);
 			cones = global_cones;
-			traffic_msg = global_traffic_msg;
-			traffic_msg_timestamp = global_traffic_msg_timestamp;
+			kiwis = global_kiwis;
 		}
 
 		// Check if intersection is reached
@@ -71,19 +84,34 @@ int32_t main(int32_t, char **)
 			}
 		}
 
-		// Check if traffic is present
-		float seconds_since_last_traffic_msg = float(cluon::time::toMicroseconds(cluon::time::now()) - traffic_msg_timestamp) / 1000000.0f;
-		bool traffic_to_right = T_LEFT < traffic_msg.relative_bearing() && traffic_msg.relative_bearing() < T_RIGHT;
+		if (intersection_reached)
+		{
+			// Check if traffic is present
+			for (uint32_t i = 0; i < kiwis.size(); i++)
+			{
+				KiwiLocation kiwi = kiwis.at(i);
+				if (T_LEFT < kiwi.relative_bearing() && kiwi.relative_bearing() < T_RIGHT)
+				{
+					last_traffic_msg_timestamp = cluon::time::now();
+					break;
+				}
 
+			}
+		}
+
+		float microseconds_since_last_traffic = cluon::time::deltaInMicroseconds(cluon::time::now(),last_traffic_msg_timestamp);
+		
+		
 		// Choose drive state
 		uint32_t drive_state;
-		if (intersection_reached && traffic_to_right && seconds_since_last_traffic_msg < T_TIME)
+		if (microseconds_since_last_traffic < T_WAIT_TIME) 
 		{
 			drive_state = STOP;
-		} else 
+		} else  
 		{
 			drive_state = DRIVE;
 		}
+		std::cout << "Drive state: " << drive_state << std::endl;
 
 		// Send drive state
 		opendlv::robo::DriveState drive_state_msg;
