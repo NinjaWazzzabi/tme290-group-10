6f240d13b550d7771607f01228f49e2ec2efd54b
Landsfadern <elias.w.burstrom@gmail.com>
2020-05-30 15:57:34 +0200

    Update kiwi_location in action-planner

--- action-planner/src/kiwi_location.hpp
+++ action-planner/src/kiwi_location.hpp
@@ -6,23 +6,64 @@
 class KiwiLocation
 {
 	static constexpr double WIDTH = 0.16; // Meters
-
+	double x_;
+	double y_;
+	double w_;
+	double h_;
 	double relative_bearing_;
 	double distance_;
 
 public:
 
-	KiwiLocation(double relative_bearing, double distance)
+	KiwiLocation(double x, double y, double w, double h, double relative_bearing, double distance)
 	{
+		x_ = x;
+		y_ = y;
+		w_ = w;
+		h_ = h;
 		relative_bearing_ = relative_bearing;
 		distance_ = distance;
 	}
 
-	KiwiLocation(uint32_t image_width, double camera_fov, uint32_t kiwi_width_imagespace, uint32_t kiwi_centre_imagespace)
+	KiwiLocation(double x, double y, double w, double h,uint32_t image_width, double camera_fov, uint32_t kiwi_width_imagespace, uint32_t kiwi_centre_imagespace)
 	{
+		x_ = x;
+		y_ = y;
+		w_ = w;
+		h_ = h;
 		double kiwi_fov = camera_fov * (double(WIDTH) / double(kiwi_width_imagespace));
 		// Using hypotenuse theorem
 		distance_ = sqrt(kiwi_fov * kiwi_fov / 4.0 - WIDTH * WIDTH / 4.0);
 		relative_bearing_ = (double(kiwi_centre_imagespace) - double(image_width) / 2.0) * (camera_fov / double(image_width));
+	}
+
+	float x()
+	{
+		return x_;
+	}
+
+	float y()
+	{
+		return y_;
+	}
+
+	float w()
+	{
+		return w_;
+	}
+	
+	float h()
+	{
+		return h_;
+	}
+
+	float distance()
+	{
+		return distance_;
+	}
+
+	float relative_bearing()
+	{
+		return relative_bearing_;
 	}
 };
